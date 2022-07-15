bfb841a825ccd599dd82303d94d9b8644a148d25
Talita Santos <tatha.talita@gmail.com>
2022-07-15 19:45:08 -0300

    feat: adiciona novo valor para a variável guardar as informacoes

--- demo01-variaveis.js
+++ demo01-variaveis.js
@@ -2,9 +2,10 @@ let nome = "Talita"
 let idade = 100
 let poder = "Super Força"
 
+nome = "Euzinha"
+poder = "Deusona"
 console.log(
     `Nome: ${nome}
     Idade: ${idade}
     Poder: ${poder}`
-
 )
